da9d396a93687def361182eca9ce4afd25ff3791
Nadeeth Lansakara <nadeeth@nadeeth.info>
2021-10-24 22:46:47 +1300

    exportImageToGallery method - Kotlin (Android)

--- android/src/main/java/com/butterflyray/plugins/exportimage/ExportBase64ImageToGalleryPlugin.kt
+++ android/src/main/java/com/butterflyray/plugins/exportimage/ExportBase64ImageToGalleryPlugin.kt
@@ -1,28 +1,47 @@
 package com.butterflyray.plugins.exportimage
 
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.insertImage
+import android.provider.SyncStateContract.Helpers.insert
+import android.util.Base64
 import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
 import com.getcapacitor.PluginMethod
 import com.getcapacitor.annotation.CapacitorPlugin
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 @CapacitorPlugin(name = "ExportBase64ImageToGallery")
 class ExportBase64ImageToGalleryPlugin : Plugin() {
+
     private val implementation = ExportBase64ImageToGallery()
-    @PluginMethod
-    fun echo(call: PluginCall) {
-        val value = call.getString("value")
-        val ret = JSObject()
-        ret.put("value", value?.let { implementation.echo(it) })
-        call.resolve(ret)
-    }
 
     @PluginMethod
     fun exportImageToGallery(call: PluginCall) {
-        val value = call.getString("data")
+        val base64String = call.getString("data")
+
+        val imageBytes = Base64.decode(base64String, Base64.DEFAULT)
+        val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.count())
+
         val ret = JSObject()
-        ret.put("success", value?.let { implementation.echo(it) })
-        ret.put("error", "no")
-        call.resolve(ret)
+        try {
+            implementation.saveImage(decodedImage, context, "DrawingGrid")
+            ret.put("success", true)
+            ret.put("error", "")
+            call.resolve(ret)
+        } catch (e: Exception) {
+            ret.put("success", true)
+            ret.put("error", e.message)
+            call.resolve(ret)
+        }
     }
 }
